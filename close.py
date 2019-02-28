7a4595f97b70d2474ce4473d8da56a34cfe4d59a
Tobin Fitzthum <fitzthum@dhcp-130-58-160-174.swarthmore.edu>
2019-02-28 16:21:37 -0500

    working with video

--- close.py
+++ close.py
@@ -1,6 +1,8 @@
 import sys
 import cv2 
 import numpy as np
+import progressbar
+from matplotlib import pyplot as plt 
 
 def labelFrame(img,size):
   font = cv2.FONT_HERSHEY_SIMPLEX
@@ -11,38 +13,51 @@ def plotFace(img,face):
   cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
 def main():
-  print("img size")
-  img = cv2.imread("nun.png")
-  height, width, _ = img.shape
+  video = cv2.VideoCapture(sys.argv[1])
+  frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+  sizes = np.zeros(frame_count,dtype=np.float32)
 
-  print("Height: {}, Width: {}".format(height,width))
+  for i in progressbar.progressbar(range(frame_count - 1)):
+    ret, img = video.read()
+    assert ret 
+    height, width, _ = img.shape
 
-  # make grayscale 
-  img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-  # load classifier 
-  classifier = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
-
-  # find faces 
-  faces = classifier.detectMultiScale(img_gray,scaleFactor=1.1,minNeighbors=5)
-  
-  largest_area = 0
-  largest_face = None 
-  for face in faces:
-    x,y,w,h = face 
-    area = w*h
-    if area > largest_area: 
-      largest_face = face
+    classifier = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
+    faces = classifier.detectMultiScale(img_gray,scaleFactor=1.1,minNeighbors=5)
     
-  if largest_face is not None:
-    x,y,w,h = face 
-    percent_size = 100 * (w * h) / (height * width)  
-    labelFrame(img,percent_size)
-    plotFace(img,face)
-  
+    largest_area = 0
+    largest_face = None 
+    for face in faces:
+      x,y,w,h = face 
+      area = w*h
+      if area > largest_area: 
+        largest_face = face
+      
+    if largest_face is not None:
+      x,y,w,h = face 
+      percent_size = 100 * (w * h) / float(height * width)  
+      sizes[i] = percent_size
 
-  cv2.imshow("img",img)
-  cv2.waitKey(0)
+      labelFrame(img,percent_size)
+      plotFace(img,face)
+    else:
+      sizes[i] = 0
+      
+    #cv2.imshow("img",img)
+    #cv2.waitKey(25)
+  
+  mean_size = np.mean(sizes)
+  median_size = np.median(sizes)
+  max_size = np.amax(sizes)
+  std_dev = np.std(sizes)
+  
+  plt.hist(sizes)
+  plt.title("Size of Largest Face in Frame")
+  plt.show()
+
+  print("Average Size: {}".format(avg_size))
 
 if __name__ == "__main__":
   main()
