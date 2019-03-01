f0fbde6833c0d6a63b159fbd69580e4f135b0a7c
Tobin Fitzthum <fitzthum@dhcp-130-58-160-174.swarthmore.edu>
2019-03-01 00:56:00 -0500

    ibis switch

--- close.py
+++ close.py
@@ -2,11 +2,10 @@ import sys
 import cv2 
 import numpy as np
 import progressbar
-import math 
 from matplotlib import pyplot as plt 
 
 def labelFrame(img,size):
-  size = math.round(size,2)
+  size = round(size,2)
   font = cv2.FONT_HERSHEY_SIMPLEX
   cv2.putText(img,"{}%".format(size),(10,30),font,1,(0,255,0),2,cv2.LINE_AA)
 
@@ -15,8 +14,10 @@ def plotFace(img,face):
   cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
 def main():
-  display = True
-  export = True
+  display = False
+  export = False
+  step = 10
+  scale = 2
 
   video = cv2.VideoCapture(sys.argv[1])
   height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
@@ -27,18 +28,23 @@ def main():
     out = cv2.VideoWriter('out.avi',fourcc,20.0,(width,height))
 
   frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-  sizes = np.zeros(frame_count,dtype=np.float32)
+  assert frame_count > 0 
+  sizes = np.zeros(frame_count/step,dtype=np.float32)
 
-  for i in progressbar.progressbar(range(frame_count - 1)):
+  for i in progressbar.progressbar(range(0,frame_count - 1,step)):
+    if not step == 1:
+      video.set(cv2.CAP_PROP_POS_FRAMES,i)
     ret, img = video.read()
     if not ret: 
-      break 
+      break
+    if scale > 1: 
+      img = cv2.resize(img,(height/scale,width/scale))
 
     img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
     classifier = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
     faces = classifier.detectMultiScale(img_gray,scaleFactor=1.1,minNeighbors=5)
-    
+
     largest_area = 0
     largest_face = None 
     for face in faces:
@@ -50,13 +56,13 @@ def main():
     if largest_face is not None:
       x,y,w,h = face 
       percent_size = 100 * (w * h) / float(height * width)  
-      sizes[i] = percent_size
+      sizes[i/step] = percent_size
 
       if display:
         labelFrame(img,percent_size)
         plotFace(img,face)
     else:
-      sizes[i] = 0
+      sizes[i/step] = 0
      
     if display:
       cv2.imshow("img",img)
@@ -64,9 +70,9 @@ def main():
     if export: 
       out.write(img)
 
-    
   video.release()
-  out.release()
+  if export:
+    out.release()
   cv2.destroyAllWindows()
 
   mean_size = np.mean(sizes)
@@ -76,7 +82,7 @@ def main():
   
   print("Average Size: {} \n Median Size: {} \n Max Size: {} \n Standard Deviation: {}".format(mean_size,median_size,max_size,std_dev))
   
-  plt.hist(sizes)
+  plt.hist(sizes,bins=20)
   plt.title("Size of Largest Face in Frame")
   plt.show()
 
